abd6f45e2f45bbc93ee10e30c0f5fea8b2a0de3e
Daniel Vieira <danielvie@gmail.com>
2023-12-21 12:52:20 +0100

    implementing linter sugestions

--- gmail.py
+++ gmail.py
@@ -1,117 +1,146 @@
+"""program to read emails and archive them"""
 
 import pickle
+import os
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import InMemoryHistory
-from prompt_toolkit import PromptSession
 
 from rich.console import Console
 from rich.progress import track
-import os
 
 history = InMemoryHistory()
 session = PromptSession(history=history)
 console = Console()
 
-class Gmail():
-    SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
+class Gmail:
+    """Class that reads the gmails ans manages logic"""
+
+    SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
     creds = None
     service = None
     items = []
     test: bool = False
     msgid = {}
-    
+
     def __init__(self) -> None:
         # get token from previous session
-        if os.path.exists('token.pickle'):
-            with open ('token.pickle', 'rb') as token:
+        if os.path.exists("token.pickle"):
+            with open("token.pickle", "rb") as token:
                 self.creds = pickle.load(token)
-                
+
         # ask login if there is no token
         if not self.creds or not self.creds.valid:
-            self.creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credential.json', self.SCOPES)
-            self.creds = flow.run_local_server(port=0)
-            with open('open.pickle', 'wb') as token:
-                pickle.dump(self.creds, token)
-            
-        self.service = build('gmail', 'v1', credentials=self.creds)
-        
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credential.json", self.SCOPES
+                )
+                self.creds = flow.run_local_server(port=0)
+                with open("token.pickle", "wb") as token:
+                    pickle.dump(self.creds, token)
+
+        self.service = build("gmail", "v1", credentials=self.creds)
+
         # get list of queries
         file = "query_list.txt"
-        
-        with open(file, "r") as f:
+
+        with open(file, "r", encoding="utf-8") as f:
             while l := f.readline():
                 self.items.append(l.replace("\n", ""))
 
     def in_test(self, value):
+        """sets module to test mode"""
         if value:
             tam = 40
-            bar = '-'*tam
-            msg_ = 'APP IN TEST!!!'
-            msg = f'[red]{msg_}[/red]'
-            console.print(bar, style='bold red')
-            console.print(bar, style='bold red')
-            console.print(f'{msg: ^{tam+len(msg_)}}')
-            console.print(bar, style='bold red')
-            console.print(bar, style='bold red')
+            barra = "-" * tam
+            msg_ = "APP IN TEST!!!"
+            msg = f"[red]{msg_}[/red]"
+            console.print(barra, style="bold red")
+            console.print(barra, style="bold red")
+            console.print(f"{msg: ^{tam+len(msg_)}}")
+            console.print(barra, style="bold red")
+            console.print(barra, style="bold red")
         self.test = value
 
-    def query(self, qry:str):
-        if qry.lower() == 'inbox':
-            print(f'query inbox')
-            results = self.service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
-            return results.get('messages', [])
+    def query(self, qry: str):
+        """calls a query for messages in gmail"""
+        if qry.lower() == "inbox":
+            print("query inbox")
+            results = (
+                self.service.users()
+                .messages()
+                .list(userId="me", labelIds=["INBOX"])
+                .execute()
+            )
+            return results.get("messages", [])
 
-        if qry.lower() == 'unread':
-            print(f'query unread')
-            results = self.service.users().messages().list(userId='me', labelIds=['UNREAD']).execute()
-            return results.get('messages', [])
+        if qry.lower() == "unread":
+            print("query unread")
+            results = (
+                self.service.users()
+                .messages()
+                .list(userId="me", labelIds=["UNREAD"])
+                .execute()
+            )
+            return results.get("messages", [])
 
-        print(f'query: {qry}')
-        results = self.service.users().messages().list(userId='me', q=qry, labelIds=['INBOX']).execute()
-        messages = results.get('messages', [])
+        print(f"query: {qry}")
+        results = (
+            self.service.users()
+            .messages()
+            .list(userId="me", q=qry, labelIds=["INBOX"])
+            .execute()
+        )
+        messages = results.get("messages", [])
         self.msgid = {}
         for m in messages:
-            self.msgid[m['id']] = True
+            self.msgid[m["id"]] = True
 
         return messages
-        
 
     def get_message_details(self, message_id):
-        msg = self.service.users().messages().get(userId='me', id=message_id).execute()
-        headers = msg['payload']['headers']
+        """get details from message from id"""
+        msg = self.service.users().messages().get(userId="me", id=message_id).execute()
+        headers = msg["payload"]["headers"]
 
-        subject = next((header['value'] for header in headers if header['name'] == 'Subject'), None)
-        sender = next((header['value'] for header in headers if header['name'] == 'From'), None)
+        subject = next(
+            (header["value"] for header in headers if header["name"] == "Subject"), None
+        )
+        sender = next(
+            (header["value"] for header in headers if header["name"] == "From"), None
+        )
         return sender, subject
 
     def get_first_query(self):
+        """get the query using `query_list.txt` values"""
         items_with_from = " ".join([f"from:'{i}'" for i in self.items])
-        return f'{{{items_with_from}}}'
+        return f"{{{items_with_from}}}"
 
     def get_messages(self):
+        """get messages from gmail"""
         value = self.get_first_query()
         yield self.query(value)
-        
+
         while True:
             # prompt new query with history
             try:
-                value = session.prompt("\nEnter a string value to search for in message titles/subjects or 'exit' to quit: ")
+                value = session.prompt("\nSearch for ... or 'exit' to quit: ")
             except KeyboardInterrupt:
                 break
 
             # Exit condition
-            if not value or value.lower() == 'exit':
+            if not value or value.lower() == "exit":
                 break
-            
+
             if value == ".":
                 yield self.query(self.get_first_query())
                 continue
-            
+
             if value == "!t":
                 self.in_test(True)
                 yield self.query(self.get_first_query())
@@ -123,32 +152,38 @@ class Gmail():
                 continue
 
             yield self.query(value)
-        
-        print('\n')
-        
+
+        print("\n")
+
     def mark_as_read(self, msg_id):
+        """mark message as read"""
         if self.test:
             _, subject = self.get_message_details(msg_id)
-            console.print(f'\n[blue]mark read for[/blue]: {subject}')
+            console.print(f"\n[blue]mark read for[/blue]: {subject}")
             return
 
-        self.service.users().messages().modify(userId='me', id=msg_id, body={'removeLabelIds': ['UNREAD']}).execute()
+        self.service.users().messages().modify(
+            userId="me", id=msg_id, body={"removeLabelIds": ["UNREAD"]}
+        ).execute()
 
     def mark_as_arquived(self, msg_id):
+        """remove label inbox from message"""
         if self.test:
             _, subject = self.get_message_details(msg_id)
-            console.print(f'\n[blue]mark arquived for[/blue]: {subject}')
+            console.print(f"\n[blue]mark arquived for[/blue]: {subject}")
             return
 
-        self.service.users().messages().modify(userId='me', id=msg_id, body={'removeLabelIds': ['INBOX']}).execute()
+        self.service.users().messages().modify(
+            userId="me", id=msg_id, body={"removeLabelIds": ["INBOX"]}
+        ).execute()
 
     def remove_messages(self, ids):
+        """call all methods to remove messages"""
         for m_id in track(ids, description="processing... "):
             # Mark as read
             self.mark_as_read(m_id)
             # Archive
             self.mark_as_arquived(m_id)
-
 
     def __del__(self) -> None:
         pass
--- main.py
+++ main.py
@@ -1,55 +1,65 @@
-# import base64
+"""Program to read emails and archive them"""
+
 import os.path
 import os
 import tempfile
 
 from gmail import Gmail
 
-def call_vim(temp_message:str) -> str:
+
+def call_vim(message: str) -> str:
+    """Function that calls vim to edit messages"""
     # create a temporary file
     with tempfile.NamedTemporaryFile(suffix=".tmp", delete=False, mode="w") as tf:
-        tf.write(temp_message)
+        tf.write(message)
         temp_file_name = tf.name
 
     # open the temporary file with vim
-    os.system(f'v {temp_file_name}')
-    
+    os.system(f"v {temp_file_name}")
+
     # read the contents of the temporary file
-    with open(temp_file_name, 'r') as tf:
-        content = tf.read()
-        
+    with open(temp_file_name, "r", encoding="utf-8") as tf:
+        res = tf.read()
+
     # delete the temporary file
     os.remove(temp_file_name)
 
-    return content
+    return res
+
 
 app = Gmail()
 app.in_test(False)
 
 for found_messages in app.get_messages():
-    
     if not found_messages:
         print("No messages found in INBOX.")
         continue
 
-    temp_message:str = ""
-    for i,m in enumerate(found_messages):
-        sender, subject = app.get_message_details(m['id'])
-        
+    temp_message: str = ""
+    for i, m in enumerate(found_messages):
+        sender, subject = app.get_message_details(m["id"])
+
+        subject = subject.replace("\n", "")
         mi = f"{i+1}: {sender} | {subject} | {m['id']}"
         temp_message += f"{mi}\n"
 
     content = call_vim(temp_message)
-    
+
     # display messages to be filtered
     list_of_msg_ids = []
-    for line in content.split('\n'): 
+    for line in content.split("\n"):
         if line:
             list_of_msg_ids.append(line.split("| ")[-1].strip())
 
-    print('\n\n')
+    print("\n\n")
     print(content)
-    choice = input(f"\nDo you want to mark these ({len(list_of_msg_ids)}) as read and archive them? (yes/No): ").strip().lower()
+    choice = (
+        input(
+            f"\nMark these ({len(list_of_msg_ids)}) as read and archive them? (yes/No): "
+        )
+        .strip()
+        .lower()
+    )
 
-    if choice == 'yes' or choice == 'y':
+    if choice in ("yes", "y"):
         app.remove_messages(list_of_msg_ids)
